4fe620634fdbe57f2a98e1b0145140693eb1ea82
orionsuperman <orionsuperman@gmail.com>
2022-04-22 13:19:03 -0500

    working bot

--- app.js
+++ app.js
@@ -1,34 +1,90 @@
+// `cp _env .env` then modify it
+// See https://github.com/motdotla/dotenv
+const config = require("dotenv").config().parsed;
+// Overwrite env variables anyways
+for (const k in config) {
+  process.env[k] = config[k];
+}
+
+let agent = undefined;
+// If you have HTTP proxy
+const httpProxyUrl = process.env.HTTP_PROXY;
+if (typeof httpProxyUrl !== 'undefined') {
+  const HttpsProxyAgent = require('https-proxy-agent');
+  agent = new HttpsProxyAgent(httpProxyUrl);
+}
+
+// Enable helpful logging for debugging
+const developerMode = true;
+const { LogLevel } = require("@slack/logger");
+const logLevel = process.env.SLACK_LOG_LEVEL || LogLevel.DEBUG;
+
+// The initialization can be deferred until App#init() call when true
+const deferInitialization = true;
+
+const thesaurize = require("thesaurize");
+
 const { App } = require("@slack/bolt");
-require("dotenv").config();
-// Initializes your app with your bot token and signing secret
 const app = new App({
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
   token: process.env.SLACK_BOT_TOKEN,
-  socketmode:true,
-  appToken: process.env.APP_TOKEN,
-  developerMode: true,
-  deferInitialization: true,
+  agent,
+  logLevel,
+  developerMode,
+  deferInitialization,
 });
 
-app.command("/thesaurize", async ({command, ack, say}) => {
-    try {
-        await ack();
-        say("Yay, the command came to my command.")
-    } catch (error) {
-        console.log("err");
-        console.error(error);
+// Request dumper middleware for easier debugging
+if (process.env.SLACK_REQUEST_LOG_ENABLED === "1") {
+  app.use(async (args) => {
+    const copiedArgs = JSON.parse(JSON.stringify(args));
+    copiedArgs.context.botToken = 'xoxb-***';
+    if (copiedArgs.context.userToken) {
+      copiedArgs.context.userToken = 'xoxp-***';
     }
-})
+    copiedArgs.client = {};
+    copiedArgs.logger = {};
+    args.logger.debug(
+      "Dumping request data for debugging...\n\n" +
+      JSON.stringify(copiedArgs, null, 2) +
+      "\n"
+    );
+    const result = await args.next();
+    args.logger.debug("next() call completed");
+    return result;
+  });
+}
+
+// ---------------------------------------------------------------
+// Start coding here..
+// see https://slack.dev/bolt/
+
+app.command("/thesaurize", async ({ logger, command, ack, say }) => {
+  await thesaurizeText({ logger, command, ack, say });
+});
+
+// ---------------------------------------------------------------
+
+async function thesaurizeText({ logger, command, ack, say }) {
+    try {
+      let mutatedText = thesaurize(command.text)
+      logger.debug(command);
+      await ack();
+      await say(`${command.user_name}: ${command.text} \n Thesaurize: ${mutatedText}`);
+    } catch (e) {
+      logger.error(e);
+      await ack(`I done borked`);
+    }
+  }
 
 (async () => {
-    try{
-        const port = 3000
-        // Start your app
-        await app.init();
-        await app.start(process.env.PORT || port);
-        console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
-    } catch(error){
-        console.log(error)
-        process.exit(1)
-    }
-  
+  try {
+    await app.init();
+    await app.start();
+    console.log("⚡️ Bolt app is running!");
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
 })();
